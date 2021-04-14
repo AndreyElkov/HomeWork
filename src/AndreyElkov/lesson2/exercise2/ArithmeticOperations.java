3b108272b69ee0cdeae71ec3e1fae9065b2aa849
AndreyElkov <a.e.elkov@gmail.com>
2021-04-14 07:31:56 +0300

    второе заданье второго урока

---
+++ src/AndreyElkov/lesson2/exercise2/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+package AndreyElkov.lesson2.exercise2;
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+
+        int naturalNumber = 11;
+        int integer = -7;
+        double b = 3.14;
+        double c = b - integer;
+        double d = 1.3 + naturalNumber;
+
+    }
+}
