be0636f1119be3f24774a0935f36e48bb2591deb
AndreyElkov <a.e.elkov@gmail.com>
2021-08-08 15:40:22 +0300

    discriminant

--- src/AndreyElkov/lesson3/exercise1/QuadraticEquation.java
+++ src/AndreyElkov/lesson3/exercise1/QuadraticEquation.java
@@ -9,22 +9,22 @@ public class QuadraticEquation {
         System.out.println("ax∧2+bx+c=0");
         System.out.println("Введите a; b; c;");
         double a, b, c;
-        double Discriminant;
+        double discriminant;
         Scanner in = new Scanner(System.in);
         a = in.nextDouble();
         b = in.nextDouble();
         c = in.nextDouble();
-        Discriminant = b * b - 4 * a * c;
+        discriminant = b * b - 4 * a * c;
         if (a == 0) {
             System.out.println("Первый коэффициент не может быть 0");
         }
-        else if (Discriminant > 0) {
+        else if (discriminant > 0) {
             double x1, x2;
-            x1 = (-b - Math.sqrt(Discriminant)) / (2 * a);
-            x2 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
             System.out.println("Уровнение имеет два корня:");
             System.out.println(+ x1 +" "+ x2);
-        } else if (Discriminant == 0) {
+        } else if (discriminant == 0) {
             double x;
             x = -b / (2 * a);
             System.out.println("Уровнение имеет один корень:");
